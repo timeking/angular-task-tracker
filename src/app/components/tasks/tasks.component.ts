bc141a282a30d4be711d0e69a163216b25528f9e
Ilya Muromtsev <let4time@gmail.com>
2023-11-10 11:27:24 +0700

    tasks service

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Task } from 'src/app/Task';
 import {TaskItemComponent} from "../task-item/task-item.component";
+import {TaskService} from "../../services/task.service";
 
 @Component({
   selector: 'app-tasks',
@@ -15,24 +16,11 @@ import {TaskItemComponent} from "../task-item/task-item.component";
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
-  tasks: Task[] = [
-    {
-      id: 1,
-      text: 'Визит к врачу',
-      day: '5 мая в 14:30',
-      reminder: true,
-    },
-    {
-      id: 2,
-      text: 'Встреча в школе',
-      day: '6 мая в 13:30',
-      reminder: true,
-    },
-    {
-      id: 3,
-      text: 'Поход в магазин',
-      day: '7 мая в 12:30',
-      reminder: false,
-    },
-  ];
+  taskService: TaskService = inject(TaskService);
+
+  tasks: Task[] = [];
+
+  constructor() {
+    this.tasks = this.taskService.getAllTasks();
+  }
 }
