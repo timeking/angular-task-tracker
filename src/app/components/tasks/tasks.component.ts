b8d30643f4cb869090b0267ce36cfc4891159870
Ilya Muromtsev <let4time@gmail.com>
2023-11-14 11:26:38 +0700

    defer on viewport + isLoading

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -3,28 +3,36 @@ import { CommonModule } from '@angular/common';
 import { Task } from 'src/app/Task';
 import {TaskItemComponent} from "../task-item/task-item.component";
 import {TaskService} from "../../services/task.service";
-import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [CommonModule, TaskItemComponent, FormsModule],
+  imports: [CommonModule, TaskItemComponent],
   template: `
     <span class="form-control">
       <input type="text"
-             [(ngModel)]="textToFilter"
-             (keyup)="onKeyUp()"
-             placeholder="Введите текст для фильтра">
+             (keyup)="onKeyUp(search.value)"
+             placeholder="Введите текст для фильтра"
+      #search>
+
+
     </span>
-    @defer {
-      @if (hasError) {
-        Ошибка запроса сервера
-      } @else {
-        @for(task of filteredTasks; track task.id) {
-            <app-task-item [task]="task" ></app-task-item>
-        } @empty {
-            Нет задач!
+    @if (isLoading) {
+    <p> Загрузка... </p>
+    }
+    @else {
+      @defer(on viewport; on timer(5s)) {
+        @if (hasError) {
+          Ошибка запроса сервера
+        } @else {
+          @for(task of filteredTasks; track task.id) {
+              <app-task-item [task]="task" ></app-task-item>
+          } @empty {
+              Нет задач по фильтру '{{ search.value }}'
+          }
         }
+      } @placeholder {
+      <p> Загрузка... </p>
       }
     }
   `,
@@ -32,25 +40,27 @@ import {FormsModule} from "@angular/forms";
 })
 export class TasksComponent {
   taskService: TaskService = inject(TaskService);
-  textToFilter: string = "";
 
   tasks: Task[] = [];
   filteredTasks: Task[] = [];
+  isLoading: boolean = false;
   hasError: boolean = false;
 
   constructor() {
+    this.isLoading = true;
     this.taskService.getAllTasks().then((taskList: Task[]) => {
       this.tasks = taskList;
       this.filteredTasks = taskList;
       this.hasError = false;
+      this.isLoading = false;
     }).catch(e => {this.hasError = true});
   }
 
-  onKeyUp() {
-    if (this.textToFilter) {
+  onKeyUp(textToFilter: string) {
+    if (textToFilter) {
       this.filteredTasks = this.tasks.filter(
         (task) => task.text.toLowerCase()
-          .includes(this.textToFilter.toLowerCase())
+          .includes(textToFilter.toLowerCase())
       );
     } else {
       this.filteredTasks = this.tasks;
